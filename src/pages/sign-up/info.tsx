eb3d140278eaf412f6b0c113c9ad8aeb64d97485
kiuuon <kiuuon29@gmail.com>
2025-02-25 20:47:21 +0900

    feat: info 페이지에서 새로고침하면 terms 페이지로 이동

--- src/pages/sign-up/info.tsx
+++ src/pages/sign-up/info.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 import NameInput from '@/components/sign-up/info/name-input';
 import BirthInput from '@/components/sign-up/info/birth-input';
@@ -10,9 +11,20 @@ import MbtiInput from '@/components/sign-up/info/mbti-input';
 import PathInput from '@/components/sign-up/info/path-input';
 import SignUpCompleteModal from '@/components/sign-up/info/sign-up-complete-modal';
 import SignUpButton from '@/components/sign-up/info/sign-up-button';
+import termsStore from '@/stores/sign-up/terms-store';
 
 function Info() {
+  const router = useRouter();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const termOfUse = termsStore((state) => state.temrOfUse);
+  const privacyPolicy = termsStore((state) => state.privacyPolicy);
+  const thirdPartyConsent = termsStore((state) => state.thirdPartyConsent);
+
+  useEffect(() => {
+    if (!termOfUse || !privacyPolicy || !thirdPartyConsent) {
+      router.push('/sign-up/terms');
+    }
+  }, [router, termOfUse, privacyPolicy, thirdPartyConsent]);
 
   return (
     <div className="flex min-h-screen flex-col bg-[#F5F5F5] p-[40px]">
