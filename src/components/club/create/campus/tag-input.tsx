cc562111a605cc6710fe90dbc8b951fd1af408b8
kiuuon <kiuuon29@gmail.com>
2025-03-19 17:35:12 +0900

    feat: 태그 삭제 버튼

--- src/components/club/create/campus/tag-input.tsx
+++ src/components/club/create/campus/tag-input.tsx
@@ -23,6 +23,10 @@ function TagInput({
   }, [defaultCampusClubType, defaultCategory]);
 
   const handleAddTag = () => {
+    if (inputValue === '') {
+      return;
+    }
+
     if (value.length >= 5) {
       alert('최대 태그 수를 초과했습니다.');
       return;
@@ -32,16 +36,26 @@ function TagInput({
     setInputValue('');
   };
 
+  const handleRemoveTag = (index: number) => {
+    const newTag = value.filter((_, i) => i !== index);
+    onChange(newTag);
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-wrap gap-2">
-        {Array.from(value)
-          .filter((tag) => tag !== '')
-          .map((tag) => (
-            <span key={tag} className="rounded-[5px] bg-[#D9D9D9] p-2">
+        {Array.from(value).map((tag, index) =>
+          tag === '' ? null : (
+            <span key={tag} className="flex gap-2 rounded-[5px] bg-[#D9D9D9] p-2">
               {tag}
+              {index !== 0 && index !== 1 && (
+                <button type="button" onClick={() => handleRemoveTag(index)}>
+                  x
+                </button>
+              )}
             </span>
-          ))}
+          ),
+        )}
       </div>
       <div className="flex gap-2">
         <input
--- src/pages/club/[clubType]/index.tsx
+++ src/pages/club/[clubType]/index.tsx
@@ -52,7 +52,20 @@ function Main() {
     }
 
     if (data?.pages[0].length === 0) {
-      return <NotPost />;
+      return (
+        <div>
+          <NotPost />
+          <button
+            type="button"
+            className="mt-4 w-full rounded-lg bg-[#FFD600] py-4 text-[#000000]"
+            onClick={() => {
+              router.push('/club/create');
+            }}
+          >
+            동아리 생성하기
+          </button>
+        </div>
+      );
     }
 
     return data?.pages.map((page) =>
