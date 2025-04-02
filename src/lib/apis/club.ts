2cdb6a39af5501352c2391a66f14753ad72d77e0
kiuuon <kiuuon29@gmail.com>
2025-04-02 14:58:58 +0900

    feat: 동아리 생성 후 자기 자신을 회장으로 동아리에 가입하기

--- src/lib/apis/club.ts
+++ src/lib/apis/club.ts
@@ -3,19 +3,29 @@ import { supabase } from './supabaseClient';
 import { fetchUserId } from './auth';
 
 export async function createClub(body: ClubType) {
+  const userId = await fetchUserId();
+  let universityId: number | null = null;
+
   if (body.type === 'campus') {
-    const userId = await fetchUserId();
     const { data } = await supabase.from('User').select('university_id').eq('id', userId).single();
-    const newBody = {
-      ...body,
-      university_id: data?.university_id,
-    };
-    await supabase.from('Club').insert([newBody]);
-  } else {
-    const newBody = {
-      ...body,
-      university_id: null,
-    };
-    await supabase.from('Club').insert([newBody]);
+    universityId = data?.university_id;
   }
+
+  const newBody = {
+    ...body,
+    university_id: universityId,
+    creator_id: userId,
+  };
+
+  const { data } = await supabase.from('Club').insert([newBody]).select();
+
+  const clubId = data?.[0]?.id;
+
+  await supabase.from('Club_User').insert([
+    {
+      user_id: userId,
+      club_id: clubId,
+      role: 'president',
+    },
+  ]);
 }
