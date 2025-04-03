18b9e545c58a488410bd09d7a335e1404fd3ae70
kiuuon <kiuuon29@gmail.com>
2025-04-03 20:22:11 +0900

    feat: 모바일 input 줌인 방지

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no, maximum-scale=1, width=device-width" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
