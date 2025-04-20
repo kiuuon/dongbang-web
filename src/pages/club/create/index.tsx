9d89613fc06e28399c99231a32901b3aa9c4b887
kiuuon <kiuuon29@gmail.com>
2025-04-20 15:28:29 +0900

    feat: 동아리 개설 타입 선택 페이지 ui 수정

--- src/pages/club/create/index.tsx
+++ src/pages/club/create/index.tsx
@@ -10,19 +10,39 @@ function Create() {
   };
 
   return (
-    <div className="flex flex-col items-start justify-center gap-[100px] px-[20px]">
+    <div className="flex flex-col items-start justify-center px-[20px] pt-[160px]">
       <ProgressBar />
       <button
         type="button"
-        className="flex h-[40px] flex-col items-start"
+        className="flex h-[140px] w-full flex-col items-start"
         onClick={() => goToClubCreate('campus/info')}
       >
-        <div className="text-[16px]">교내 동아리</div>
-        <div className="text-[12px] text-[#D5E488]">학교내에서 활동하는 동아리</div>
+        <div className="text-bold32 mt-[12px] flex h-[38px] items-center justify-center">교내 동아리</div>
+        <div className="text-regular16 h-[19px] text-secondary">학교내에서 활동하는 동아리</div>
+        <div className="mt-[8px] h-[42px]">
+          <div className="text-regular12 h-[14px] text-left text-gray2">
+            같은 학교 친구들과의 활동이 더욱 가까워집니다.
+          </div>
+          <div className="text-regular12 h-[14px] text-left text-gray2">
+            동방은 교내 동아리만의 소속감과 일상적인 운영을
+          </div>
+          <div className="text-regular12 h-[14px] text-left text-gray2">편리하게 이어갈 수 있도록 도와드립니다.</div>
+        </div>
       </button>
-      <button type="button" className="flex h-[40px] flex-col items-start" onClick={() => goToClubCreate('union/info')}>
-        <div className="text-[16px]">연합 동아리</div>
-        <div className="text-[12px] text-[#D5E488]">전국/지역에서 다른 학교 학생들과 활동하는 동아리</div>
+      <button
+        type="button"
+        className="flex h-[140px] w-full flex-col items-start"
+        onClick={() => goToClubCreate('campus/info')}
+      >
+        <div className="text-bold32 mt-[12px] flex h-[38px] items-center justify-center">연합 동아리</div>
+        <div className="text-regular16 h-[19px] text-secondary">전국/지역에서 다른 학교 학생들과 활동하는 동아리</div>
+        <div className="mt-[8px] h-[42px]">
+          <div className="text-regular12 h-[14px] text-left text-gray2">서로 다른 캠퍼스, 하나의 동아리.</div>
+          <div className="text-regular12 h-[14px] text-left text-gray2">
+            동방은 연합 동아리만의 특별한 교류와 협력을
+          </div>
+          <div className="text-regular12 h-[14px] text-left text-gray2">더 가까이 이어갈 수 있도록 지원합니다.</div>
+        </div>
       </button>
     </div>
   );
