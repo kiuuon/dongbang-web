df57851aad6db26b20e1e3319d42499054a2cd24
kiuuon <kiuuon29@gmail.com>
2025-04-20 16:25:45 +0900

    style: 동아리 개설 페이지 ui 부분 수정

--- src/components/club/create/progress-bar.tsx
+++ src/components/club/create/progress-bar.tsx
@@ -7,7 +7,7 @@ function ProgressBar() {
   const { pathname } = router;
 
   return (
-    <div className="fixed left-0 top-0 z-10 flex w-full flex-col gap-[8px] bg-white px-[20px] pt-[12px]">
+    <div className="fixed left-0 top-0 z-50 flex w-full flex-col gap-[8px] bg-white px-[20px] pt-[12px]">
       <BackButton />
       <div className="t-0 relative flex w-full items-center justify-between pl-[34px] pr-[21px] pt-[5px]">
         <div className={`h-[13px] w-[13px] rounded-[16px] ${pathname === '/club/create' ? 'bg-gray2' : 'bg-gray0'}`} />
@@ -20,9 +20,15 @@ function ProgressBar() {
         <div className="absolute left-[34px] right-[21px] z-[-1] h-[1px] bg-gray0" />
       </div>
       <div className="flex w-full justify-between">
-        <div className="text-regular12 text-gray2">동아리 개설하기</div>
-        <div className="text-regular12 text-gray2">동아리 정보 입력</div>
-        <div className="text-regular12 text-gray2">페이지 설정</div>
+        <div className={pathname === '/club/create' ? 'text-bold12 text-black' : 'text-regular12 text-gray2'}>
+          동아리 개설하기
+        </div>
+        <div className={pathname.split('/')[4] === 'info' ? 'text-bold12 text-black' : 'text-regular12 text-gray2'}>
+          동아리 정보 입력
+        </div>
+        <div className={pathname.split('/')[4] === 'detail' ? 'text-bold12 text-black' : 'text-regular12 text-gray2'}>
+          페이지 설정
+        </div>
       </div>
     </div>
   );
