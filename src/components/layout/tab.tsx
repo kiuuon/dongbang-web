e027dbfe0c79805c37bcb942be526e2a3fa97447
kiuuon <kiuuon29@gmail.com>
2025-04-23 16:55:09 +0900

    style: tab style 수정

--- src/components/layout/tab.tsx
+++ src/components/layout/tab.tsx
@@ -11,44 +11,34 @@ function Tab() {
   const router = useRouter();
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-50 flex h-[70px] justify-around border-t border-gray0 bg-primary">
+    <nav className="fixed bottom-0 left-0 right-0 z-50 flex h-[62px] justify-around border-t border-gray0 bg-white">
       <Link href="/" className="mt-[12px] flex w-[40px] flex-col items-center">
-        <HomeIcon color={router.pathname.startsWith('/post') ? '#5C4A25' : '#A17C4A'} />
-        <div
-          className={`text-regular12 ${router.pathname.startsWith('/post') ? 'text-tertiary_dark' : 'text-tertiary'}`}
-        >
+        <HomeIcon color={router.pathname.startsWith('/post') ? '#FFE6A1' : '#B4B4B4'} />
+        <div className={`text-regular10 ${router.pathname.startsWith('/post') ? 'text-secondary' : 'text-gray1'}`}>
           홈
         </div>
       </Link>
       <Link href="/explore" className="mt-[12px] flex w-[40px] flex-col items-center">
-        <SearchIcon color={router.pathname === '/explore' ? '#5C4A25' : '#A17C4A'} />
-        <div
-          className={`text-regular12 ${router.pathname.startsWith('/explore') ? 'text-tertiary_dark' : 'text-tertiary'}`}
-        >
+        <SearchIcon color={router.pathname === '/explore' ? '#FFE6A1' : '#B4B4B4'} />
+        <div className={`text-regular10 ${router.pathname.startsWith('/explore') ? 'text-secondary' : 'text-gray1'}`}>
           검색
         </div>
       </Link>
       <Link href="/club" className="mt-[12px] flex w-[40px] flex-col items-center">
-        <ClubIcon color={router.pathname === '/club' ? '#5C4A25' : '#A17C4A'} />
-        <div
-          className={`text-regular12 ${router.pathname.startsWith('/club') ? 'text-tertiary_dark' : 'text-tertiary'}`}
-        >
+        <ClubIcon color={router.pathname === '/club' ? '#FFE6A1' : '#B4B4B4'} />
+        <div className={`text-regular10 ${router.pathname.startsWith('/club') ? 'text-secondary' : 'text-gray1'}`}>
           동아리
         </div>
       </Link>
       <Link href="/interact" className="mt-[12px] flex w-[40px] flex-col items-center">
-        <InteractIcon color={router.pathname === '/interact' ? '#5C4A25' : '#A17C4A'} />
-        <div
-          className={`text-regular12 ${router.pathname.startsWith('/interact') ? 'text-tertiary_dark' : 'text-tertiary'}`}
-        >
+        <InteractIcon color={router.pathname === '/interact' ? '#FFE6A1' : '#B4B4B4'} />
+        <div className={`text-regular10 ${router.pathname.startsWith('/interact') ? 'text-secondary' : 'text-gray1'}`}>
           교류
         </div>
       </Link>
       <Link href="/mypage" className="mt-[12px] flex w-[60px] flex-col items-center">
-        <ProfileIcon color={router.pathname.startsWith('/mypage') ? '#5C4A25' : '#A17C4A'} />
-        <div
-          className={`text-regular12 ${router.pathname.startsWith('/mypage') ? 'text-tertiary_dark' : 'text-tertiary'}`}
-        >
+        <ProfileIcon color={router.pathname.startsWith('/mypage') ? '#FFE6A1' : '#B4B4B4'} />
+        <div className={`text-regular10 ${router.pathname.startsWith('/mypage') ? 'text-secondary' : 'text-gray1'}`}>
           마이페이지
         </div>
       </Link>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,7 +5,7 @@ import GoogleLoginButton from '@/components/login/GoogleLoginButton';
 
 function Login() {
   return (
-    <div className="flex h-screen w-screen flex-col items-center gap-[172px] bg-primary px-[48px] pt-[210px]">
+    <div className="flex h-screen w-screen flex-col items-center gap-[172px] bg-secondary px-[48px] pt-[210px]">
       <Image src="/images/logo.png" alt="로고" width={140} height={140} />
       <div className="flex w-full flex-col gap-[12px]">
         <KakaoLoginButton />
