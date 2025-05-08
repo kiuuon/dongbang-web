619bd39c2cc30573820d40012dfcf01ccfbd8cbe
kiuuon <kiuuon29@gmail.com>
2025-05-08 21:02:12 +0900

    chore: _app.tsx에 안쓰는 useRef 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
